ee79686ecd06b40c8702f86833d91e1ef2c496f6
ivan medvedev <vmedvedev1017@gmail.com>
2026-04-29 21:35:33 +0300

    better diffsizes

--- cpp/thesis/diffsizes/run.sh
+++ cpp/thesis/diffsizes/run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-perf mem -t load record -c50 \
-  # -e cache-misses,cache-references,ref-cycles                       \
-  build/payload_tree.debra                                          \
-  -json-file thesis/diffsizes/one.json
-  # -result-json thesis/diffsizes/one.res.json
+rm ctl ack
+mkfifo ctl ack
+
+perf stat --control fifo:ctl \
+  -e instructions,cache-references,cache-misses,L1-dcache-loads,L1-dcache-load-misses  \
+  -- build/payload_tree.debra -json-file thesis/diffsizes/one.json
